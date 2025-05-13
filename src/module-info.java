a6e933f5f849de348db53db46e08f82c9ed71b51
Sandy <sandyrajalamsyah@gmail.com>
2025-05-13 22:12:40 +0700

    chore: update module info

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,12 @@
 module RUindows {
 	opens style;
-	opens style.resources;
+//	opens style.resources;
+	opens style.resources.icons;
+	opens style.resources.pictures;
+	opens style.resources.audios;
 	opens main;
 	requires javafx.graphics;
 	requires javafx.controls;
+	requires javafx.media;
+	requires javafx.swing;
 }
